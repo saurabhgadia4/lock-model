62925874f01d6f57cc6eb135cca15adbf73a6d8b
Cyrille Artho <c.artho@aist.go.jp>
2015-09-02 10:36:21 +0900

    modified:   rtems/LockSet.java: Print lock desc.

--- rtems/LockSet.java
+++ rtems/LockSet.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 public class LockSet {
 	public static ArrayList<Object> locks =
 		new ArrayList<Object>(); // to get lock ID
-	public static ArrayList<String> lockDesc = new ArrayList<String>();
+//	public static ArrayList<String> lockDesc = new ArrayList<String>();
 	public int heldLocks = 0; // bit set for 32 locks
 
 	private int getLockID(Object lock, String description) {
 		int idx = locks.indexOf(lock);
 		if (idx == -1) {
 			locks.add(lock);
-			lockDesc.add(description);
+//			lockDesc.add(description);
 			idx = locks.size() - 1;
+			System.out.println("Lock " + idx + " is " + description);
 		}
 		assert (idx < 32);
 		return idx;
