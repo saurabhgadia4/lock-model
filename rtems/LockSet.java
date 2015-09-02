745d3d4c558bd9b9fb51fab8673bfa45d0619852
Cyrille Artho <c.artho@aist.go.jp>
2015-09-02 10:00:00 +0900

    new file:   rtems/LockSet.java: Simple lock set implementation.

---
+++ rtems/LockSet.java
@@ -0,0 +1,30 @@
+package rtems;
+
+import java.util.ArrayList;
+
+public class LockSet {
+	public static ArrayList<Object> locks =
+		new ArrayList<Object>(); // to get lock ID
+	public int heldLocks = 0; // bit set for 32 locks
+
+	private int getLockID(Object lock) {
+		int idx = locks.indexOf(lock);
+		if (idx == -1) {
+			locks.add(lock);
+			idx = locks.size() - 1;
+		}
+		return idx;
+	}
+
+	public void addLock(Object lock) {
+		heldLocks |= (2 << getLockID(lock));
+	}
+
+	public void removeLock(Object lock) {
+		heldLocks &= ~(2 << getLockID(lock));
+	}
+
+	public int intersect(LockSet that) {
+		return heldLocks & that.heldLocks;
+	}
+}
